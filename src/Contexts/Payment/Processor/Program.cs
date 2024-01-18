ca978cdef83aae0b5d9f9bebc3d1773fc66e0b70
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2024-01-18 02:41:56 -0300

    setup: add projects

---
+++ src/Contexts/Payment/Processor/Program.cs
@@ -0,0 +1,7 @@
+using MCIO.Demos.Store.Payment.Processor;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
