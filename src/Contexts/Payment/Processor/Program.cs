36620a7e0253b20f9e6592fd3406db3d0f26795f
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2024-01-20 00:50:07 -0300

    feat: add all services for app components

--- src/Contexts/Payment/Processor/Program.cs
+++
@@ -1,7 +0,0 @@
-using MCIO.Demos.Store.Payment.Processor;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
-
-var host = builder.Build();
-host.Run();
