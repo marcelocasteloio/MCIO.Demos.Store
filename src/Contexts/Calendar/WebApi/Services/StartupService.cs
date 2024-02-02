9ae41722779138248f097821b7ee6d9c63c6f80b
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2024-02-01 22:30:04 -0300

    feat: add calendar service

---
+++ src/Contexts/Calendar/WebApi/Services/StartupService.cs
@@ -0,0 +1,25 @@
+﻿namespace MCIO.Demos.Store.Calendar.WebApi.Services;
+
+public class StartupService
+{
+    // Properties
+    public static bool HasStarted { get; private set; }
+
+    // Constructors
+    static StartupService()
+    {
+        HasStarted = false;
+    }
+    protected StartupService() { }
+
+    // Public Methods
+    public static Task TryStartupAsync(
+        IServiceProvider serviceProvider
+    )
+    {
+        HasStarted = true;
+
+        return Task.CompletedTask;
+    }
+}
+
